5fe88f40f314434acbadf48d54cefaafd4ade0c3
taaigo <taigo5@yahoo.com>
2023-09-18 14:51:49 +0200

    fixed scroll jumping issue

--- backend/import-head.php
+++ backend/import-head.php
@@ -3,6 +3,4 @@ echo '
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="/rhythm-hub/frontend/javascript/sticky-navbar.js" defer></script>
-
 ';
--- frontend/elements/navbar.php
+++ frontend/elements/navbar.php
@@ -1,4 +1,4 @@
-<nav id="navbar">
+<nav id="navbar" class="sticky">
     <ul>
         <li><a class="nav" href="/rhythm-hub/frontend/markup/main_page/">Home</a></li>
         <li><a class="nav" href="/rhythm-hub/frontend/markup/songs/">Songs</a></li>
